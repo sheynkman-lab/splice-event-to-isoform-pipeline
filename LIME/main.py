af5d35e1ee5a4a527df9033a560a6b154fc1df3d
siyer23 <siyer@smith.edu>
2022-11-21 12:11:44 -0500

    git

--- LIME/main.py
+++ LIME/main.py
@@ -4,55 +4,8 @@ import click
 from readData import *
 from gtfparse import read_gtf
 
-def load():
 
-    ## step 1: read rMATS data
-    srSource = "rmats"
-    lrSource = "pacbio"
-    rmatsPath = "/Volumes/sheynkman/projects/shay_thesis/input-data/01_ips-s1s2_rmats-out_input"
-    type = "JCEC"
-    eventTypes = ("se", "mxe", "a3ss", "a5ss")
-    eventDict = {}
-
-    se = loadSE(rmatsPath, type)
-    mxe = loadMXE(rmatsPath, type)
-    a3ss = loadA3SS(rmatsPath, type)
-    a5ss = loadA5SS(rmatsPath, type)
-
-    output_folder = "/Volumes/sheynkman/projects/shay_thesis/output/01_intermediate-output/"
-
-    lrannotpath = "/Volumes/sheynkman/projects/shay_thesis/input-data/02_long-read-EC-WTC11-1_input/01_annotation/EC.gtf"
-    # lrannotpath_2 = "/Volumes/sheynkman/projects/shay_thesis/input-data/02_long-read-EC-WTC11-1_input/01_annotation/WTC11-1.gtf"
-    lrannot = loadLRannot(lrannotpath)
-
-    quantpath_c1 = "/Volumes/sheynkman/projects/shay_thesis/input-data/02_long-read-EC-WTC11-1_input/02_quantification/WTC11-1.tsv"
-    quantpath_c2 = "/Volumes/sheynkman/projects/shay_thesis/input-data/02_long-read-EC-WTC11-1_input/02_quantification/EC.tsv"
-    
-
-    count_column_name_c1 = "rep1ENCSR507JOF"
-    count_column_name_c2 = "rep1ENCSR148IIG"
-    
-    lrquant_c1 = loadLRquant(quantpath_c1, count_column_name_c1, "WTC11-1")
-    lrquant_c2 = loadLRquant(quantpath_c2, count_column_name_c2, "EC")
-
-     # se.to_csv((output_folder + "01_short-read_pandas/se.csv"), index = False)
-    # mxe.to_csv((output_folder + "01_short-read_pandas/mxe.csv"), index = False)
-    # a3ss.to_csv((output_folder + "01_short-read_pandas/a3ss.csv"), index = False)
-    # a5ss.to_csv((output_folder + "01_short-read_pandas/a5ss.csv"), index = False)
-    # lrannot.to_csv((output_folder + "02_long-read_pandas/lr_annotation-file.csv"), index = False)
-    # lrquant_c1.to_csv((output_folder + "02_long-read_pandas/lr_quantification_c1.csv"), index = False)
-    # lrquant_c2.to_csv((output_folder + "02_long-read_pandas/lr_quantification_c2.csv"), index = False)
-
-    lr_alldata = mergeAnnotQuants(lrannot, lrquant_c1, lrquant_c2)
-
-    #lr_alldata.to_csv((output_folder + "lr_annot-quant.csv"), index = False)
-
-    
-    #JunctionDict = getLRJunctionDict(lr_alldata) 
-    # addJunctionsToTable(lr_alldata, JunctionDict)
-    # map(lr_alldata, [se, mxe, a3ss, a5ss])
-
-@click.command()
+@click.command("load_data")
 @click.option('--condition1', required = True, help = "specify name of condition 1")
 @click.option('--condition2', required = True, help = "specify name of condition 2")
 @click.option('--rmats_out_folder', required = True, type = click.Path(exists = True, path_type=Path), help = "Path to output of rMATS")
@@ -77,5 +30,9 @@ def main(condition1: str, condition2: str, rmats_out_folder: Path, type: str, c1
     lrquant_c2 = loadLRquant(c2quant, c2_quantcol, condition2)
 
     lr_alldata = mergeAnnotQuants(lrannot, lrquant_c1, lrquant_c2)
+
+    #JunctionDict = getLRJunctionDict(lr_alldata) 
+    # addJunctionsToTable(lr_alldata, JunctionDict)
+    # map(lr_alldata, [se, mxe, a3ss, a5ss])
     
 main()
